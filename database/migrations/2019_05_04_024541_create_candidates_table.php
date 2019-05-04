2754dafdad4018e9e32e187226f2fd1747627bb6
deli.oona <deli.oona@gmail.com>
2019-05-04 09:53:34 +0700

    add migration for candidates table

---
+++ database/migrations/2019_05_04_024541_create_candidates_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCandidatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('candidates', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 100);
+            $table->string('gender', 1);
+            $table->string('email', 100);
+            $table->string('phone', 20);
+            $table->string('cv', 50);
+            $table->string('status', 20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('candidates');
+    }
+}
