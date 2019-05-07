abae59cfebb880dbb1ddcc2d3fa8c6298198d7e9
deli.oona <deli.oona@gmail.com>
2019-05-07 09:16:06 +0700

    setup all routings

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,12 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Auth::routes(['register' => false]);
+
+Route::get('admin', 'CandidateController@index')->name('index')->middleware('auth');
+Route::get('datatable', 'CandidateController@datatable')->name('datatable')->middleware('auth');
+Route::get('/', 'CandidateController@create')->name('create');
+Route::post('/', 'CandidateController@store')->name('store');
+Route::get('{candidate}', 'CandidateController@show')->name('show');
+Route::get('{candidate}/approve', 'CandidateController@approve')->name('approve');
+Route::get('{candidate}/reject', 'CandidateController@reject')->name('reject');
