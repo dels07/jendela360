5cb2c094cf7a46f2e83efef7f470e5b8de46515e
deli.oona <deli.oona@gmail.com>
2019-05-07 09:16:42 +0700

    add validation for candidate form

---
+++ app/Http/Requests/CandidateFormRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CandidateFormRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name'   => 'required',
+            'gender' => 'required|in:M,F',
+            'email'  => 'required|email',
+            'phone'  => 'required|min:11',
+            'file'   => 'required|mimes:pdf|max:5000' // must be pdf with 5 Mb max size
+        ];
+    }
+}
