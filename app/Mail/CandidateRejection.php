68f560d02cac921388a7242b465c3c124d369da6
deli.oona <deli.oona@gmail.com>
2019-05-07 09:17:03 +0700

    add candidate rejection email

---
+++ app/Mail/CandidateRejection.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Mail;
+
+use App\Candidate;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class CandidateRejection extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $candidate;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(Candidate $candidate)
+    {
+        $this->candidate = $candidate;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->markdown('emails.candidates.rejection');
+    }
+}
---
+++ resources/views/emails/candidates/rejection.blade.php
@@ -0,0 +1,8 @@
+@component('mail::message')
+# Aplikasi ditolak
+
+Kami memohon maaf karena tidak dapat memproses aplikasi anda.
+
+Terima kasih,<br>
+{{ config('app.name') }}
+@endcomponent
